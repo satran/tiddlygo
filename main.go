19785c0b107c50d99ce8bce2b593ab584a7fc59f
Satyajit Ranjeev <s@ranjeev.in>
2021-11-05 17:14:18 +0100

    Fix upload

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"crypto/sha256"
 	"crypto/subtle"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -28,15 +27,37 @@ func handler(fileHandler http.Handler) http.HandlerFunc {
 			w.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, OPTIONS, CONNECT, PUT, DAV, dav")
 			w.Header().Set("X-Api-Access-Type", "file")
 			w.Header().Set("DAV", "tw5/put")
+
 		case http.MethodGet:
 			fileHandler.ServeHTTP(w, r)
-		case http.MethodPut:
-			if r.Header.Get("Content-Type") == "multipart/form-data" {
-				if err := saveAttachment(r); err != nil {
-					http.Error(w, err.Error(), http.StatusConflict)
-				}
+
+		case http.MethodPost:
+			if err := r.ParseMultipartForm(0); err != nil {
+				log.Println(err)
+				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
 			}
+			file, meta, err := r.FormFile("file")
+			if err != nil {
+				log.Println(err)
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return 
+			}
+			defer file.Close()
+			f, err := os.OpenFile(meta.Filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			defer f.Close()
+			if _, err := io.Copy(f, file); err != nil {
+				log.Println(err)
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+		case http.MethodPut:
 			file := strings.Trim(r.URL.Path, "/")
 			if file == "" {
 				file = "index.html"
@@ -56,28 +77,6 @@ func handler(fileHandler http.Handler) http.HandlerFunc {
 		}
 		log.Println(r.Method, r.URL)
 	}
-}
-
-func saveAttachment(r *http.Request) error {
-	if err := r.ParseMultipartForm(10 << 20); err != nil {
-		log.Println(err)
-		return err
-	}
-	file, meta, err := r.FormFile("file")
-	if err != nil {
-		log.Printf("Error Retrieving the File from form: %s", err)
-		return err
-	}
-	defer file.Close()
-	f, err := os.OpenFile(meta.Filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
-	if err != nil {
-		return fmt.Errorf("error creating object file: %w", err)
-	}
-	defer f.Close()
-	if _, err := io.Copy(f, file); err != nil {
-		return fmt.Errorf("error writing object: %w", err)
-	}
-	return nil
 }
 
 func basicAuth(next http.HandlerFunc, username string, password string) http.HandlerFunc {
